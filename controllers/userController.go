5a67a4538508d78d27055b65e447fa21728f275a
David Johan Hasiholan Parhusip <david.johanhp@gmail.com>
2023-03-09 22:13:44 +0700

    add middleware on routes

--- controllers/userController.go
+++ controllers/userController.go
@@ -3,39 +3,38 @@ package controllers
 import (
 	"attendance-payroll-app/initializers"
 	"attendance-payroll-app/models"
+	"attendance-payroll-app/services"
+
 	// "attendance-payroll-app/services"
 	"net/http"
 	"os"
 	"time"
-
-	"github.com/go-playground/validator/v10"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func UpdateUser(c *gin.Context){
+func UpdateUser(c *gin.Context) {
 	id := c.Param("id")
 	user := models.User{}
 	db_user := models.User{}
 
 	c.Bind(&user) // from fe
 
-	initializers.DB.First(&db_user,id) // from database
+	initializers.DB.First(&db_user, id) // from database
 	initializers.DB.Model(&db_user).Updates(user)
-	
+
 }
 
-
-func DeleteUser(c *gin.Context){
+func DeleteUser(c *gin.Context) {
 	id := c.Param("id")
 	user := models.User{}
 	initializers.DB.Delete(&user, id)
 	c.Status(200)
 }
 
-func RetriveUsers(c *gin.Context){
+func RetrieveUsers(c *gin.Context) {
 	// get all data users
 	users := []models.User{}
 	initializers.DB.Find(&users)
@@ -43,67 +42,15 @@ func RetriveUsers(c *gin.Context){
 }
 
 func CreateUser(c *gin.Context) {
-	// get data from body
-	body := models.User{}
-
-	if c.Bind(&body) != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Failed to read body",
+	user, err, status := services.CreateUser(c)
+	if err != "" {
+		c.JSON(status, gin.H{
+			"message": err,
 		})
-
-		return
+	} else {
+		c.JSON(status, user)
 	}
-
-	// hash password
-	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
-
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Failed to hash password",
-		})
-
-		return
-	}
-
-	// validate input
-	validate := validator.New()
-	err = validate.Struct(body)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": err.Error(),
-		})
-
-		return
-	}
-
-	// create user
-	body.Password = string(hash)
-	result := initializers.DB.Create(&body)
-
-	if result.Error != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Failed to create user",
-		})
-
-		return
-	}
-
-	//return
-	c.JSON(http.StatusOK, gin.H{
-		"user": body,
-	})
 }
-
-// func CreateUser(c *gin.Context) {
-// 	user, err, status := services.CreateUser(c)
-// 	if (err != ""){
-// 		c.JSON(status, gin.H{
-// 			"message": err,
-// 		})
-// 	} else{
-// 		c.JSON(status, user)
-// 	}
-// }
 
 func Login(c *gin.Context) {
 	// get email and password
